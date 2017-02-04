f9058bee776ab4defc6d7415090b53b6cb0924a5
spaww <the.spaww@gmail.com>
2017-02-04 00:54:37 -0200

    piproxy - Separação de deploy e configuração

--- modules/piproxy/manifests/config.pp
+++ modules/piproxy/manifests/config.pp
@@ -4,18 +4,45 @@
 #
 # Copyright 2017 Your name here, unless otherwise noted.
 #
-class proxy::config inherits piproxy {
+class piproxy::config inherits piproxy {
   file { '/usr/local/etc/zabbix_proxy.conf':
     ensure  => present,
     owner   => 'zabbix',
     group   => 'zabbix',
     mode    => '0644',
     replace => true,
-    content => template('proxy/zabbix.proxy.conf.erb'),
+    content => template('piproxy/zabbix.proxy.conf.erb'),
     notify  => Service['zabbix-proxy'],
   }
+  file { '/usr/local/etc/zabbix_agentd.conf':
+    ensure  => present,
+    owner   => 'zabbix',
+    group   => 'zabbix',
+    mode    => '0644',
+    replace => true,
+    content => template('piproxy/zabbix.agentd.conf.erb'),
+    notify  => Service['zabbix-proxy'],
+  }
+  file { '/usr/local/etc/zabbix_agentd.conf.d':
+    ensure => directory,
+    owner   => 'zabbix',
+    group   => 'zabbix',
+  }
+  file { '/usr/local/etc/zabbix_proxy.conf.d':
+    ensure => directory,
+    owner   => 'zabbix',
+    group   => 'zabbix',
+  }
+  # Configuracao do banco de dados do proxy ------------------------------------
   file { '/var/lib/sqlite/':
     ensure => directory,
+  }
+
+  remote_file {'/var/lib/sqlite/zabbixProxy.db':
+    remote_location => "https://github.com/SpawW/zrasp/raw/master/${$zabbix_version}/db/zabbix3.2.db",
+    mode            => '0755',
+    owner   => 'zabbix',
+    group   => 'zabbix',
   }
 
 }
--- modules/piproxy/manifests/deploy.pp
+++ modules/piproxy/manifests/deploy.pp
@@ -4,7 +4,7 @@
 #
 # Copyright 2017 Your name here, unless otherwise noted.
 #
-class proxy::deploy inherits piproxy {
+class piproxy::deploy inherits piproxy {
 
   package { 'wget':
     ensure => present,
@@ -14,37 +14,31 @@ class proxy::deploy inherits piproxy {
   }
 
   remote_file {'/usr/local/bin/zabbix_get':
-    remote_location => "https://github.com/SpawW/zrasp/raw/master/${$zabbix_version}/bin/zabbix_get"
+    remote_location => "https://github.com/SpawW/zrasp/raw/master/${$zabbix_version}/bin/zabbix_get",
     mode            => '0644',
     owner           => 'zabbix',
     group           => 'zabbix',
   }
 
   remote_file {'/usr/local/bin/zabbix_sender':
-    remote_location => "https://github.com/SpawW/zrasp/raw/master/${$zabbix_version}/bin/zabbix_sender"
+    remote_location => "https://github.com/SpawW/zrasp/raw/master/${$zabbix_version}/bin/zabbix_sender",
     mode            => '0644',
     owner           => 'zabbix',
     group           => 'zabbix',
   }
 
   remote_file {'/usr/local/sbin/zabbix_agentd':
-    remote_location => "https://github.com/SpawW/zrasp/raw/master/${$zabbix_version}/bin/zabbix_agentd"
+    remote_location => "https://github.com/SpawW/zrasp/raw/master/${$zabbix_version}/bin/zabbix_agentd",
     mode            => '0644',
     owner           => 'zabbix',
     group           => 'zabbix',
   }
 
   remote_file {'/usr/local/sbin/zabbix_proxy':
-    remote_location => "https://github.com/SpawW/zrasp/raw/master/${$zabbix_version}/bin/zabbix_proxy"
+    remote_location => "https://github.com/SpawW/zrasp/raw/master/${$zabbix_version}/bin/zabbix_proxy",
     mode            => '0644',
     owner           => 'zabbix',
     group           => 'zabbix',
   }
-
-  remote_file {'/var/lib/sqlite/zabbixProxy.db':
-    remote_location => "https://github.com/SpawW/zrasp/raw/master/${$zabbix_version}/db/zabbix3.2.db"
-    mode            => '0755',
-    owner   => 'zabbix',
-    group   => 'zabbix',
-  }
+  
 }
